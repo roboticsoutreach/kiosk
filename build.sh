7a65445bc1bea3059305c2980fbe12bc77eb9443
WillB97 <will@rudi.org.uk>
2023-03-11 16:47:23 +0000

    Remove kit specific parts

--- build.sh
+++ build.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-SROBO_NAME="Student Robotics OS"
-SROBO_VERSION="$(git describe --tags)"
-
 docker run --rm --privileged \
     -v /dev:/dev \
     -v ${PWD}:/build \
     mkaczanowski/packer-builder-arm:latest \
     build \
-    -var "SROBO_NAME=${SROBO_NAME}" \
-    -var "SROBO_VERSION=${SROBO_VERSION}" \
     pi.json
--- files/user/srobo.sh
+++
@@ -1,2 +0,0 @@
-# Stop OpenCV using gstreamer in all sessions
-export OPENCV_VIDEOIO_PRIORITY_GSTREAMER=0
